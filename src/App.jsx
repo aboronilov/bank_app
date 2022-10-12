19465182b5dc5d8b9653a8e6415416b215b9f41e
Anatoly <boronilov@gmail.com>
2022-10-12 15:32:17 +0200

    ptoject structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import styles from "./style";
+
+import { 
+  Navbar,
+  Billing,
+  Business,
+  Button,
+  CardDeal,
+  Clients,
+  CTA,
+  FeedbackCard,
+  Footer,
+  GetStarted,
+  Hero,
+  Stats,
+  Testimonials 
+} from './components';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="bg-primary w-full overflow-hidden">
+      {/* NAVBAR */}
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+      {/* HERO */}
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+      {/* CONTENT */}
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
 
-export default App
+    </div>
+  );
+};
+
+export default App;
