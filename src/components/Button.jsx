76bf8ffb7df375c77b75d3a748f82380ebf438ed
Anatoly <boronilov@gmail.com>
2022-10-14 15:50:04 +0200

    CardDeal

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ styles }) => {
   return (
-    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-md cursor-pointer`}>
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-xl cursor-pointer transition duration-300 hover:opacity-80 hover:shadow-inner`}>
       Get Started
     </button>
   );
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
 const CardDeal = () => {
   return (
-    <div>CardDeal</div>
-  )
-}
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Find a better card deal <br className="sm:block hidden" /> in few easy
+          steps.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Whether you have just started to learn, or you are already an expert
+          in the world of finance, business and investments, these resources
+          will help you stay on top of the game
+        </p>
+        <Button styles="mt-10"/>
+      </div>
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className="w-full h-full md:w-3/4 md:h-3/4"/>
+      </div>
+    </section>
+  );
+};
 
-export default CardDeal
+export default CardDeal;
