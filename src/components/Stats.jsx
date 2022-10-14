ce7a23c111bb9549bcb5b5ee66bb52319251cbb2
Anatoly <boronilov@gmail.com>
2022-10-14 11:52:54 +0200

    business section

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -3,13 +3,13 @@ import {stats} from "../constants";
 
 const Stats = () => {
   return (
-    <section className={`${styles.flexCenter} flex sm:mb-20 mb-6 flex-row `}>
+    <section className={`${styles.flexCenter} flex sm:mb-20 mb-6 md:flex-row flex-col`}>
       {stats.map(item => (
         <div key={item.id} className="flex items-center flex-1 justify-center m-3">
-          <h4 className="font-poppins font-semibold md:text-2xl sm:text-[15px] xs:text-[12px] text-white">
+          <h4 className="font-poppins font-semibold md:text-2xl text-[30px] xs:mt-4  text-white">
             {item.value}
           </h4>
-          <p className="font-poppins font-normal md:text-2xl sm:text-[15px] xs:text-[12px] xs:leading-[15px] text-gradient uppercase ml-3">
+          <p className="font-poppins font-normal md:text-2xl text-[30px] xs:mt-4 text-gradient uppercase ml-3">
             {item.title}
           </p>
         </div>
